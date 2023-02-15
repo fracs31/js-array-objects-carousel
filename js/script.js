cb4b38ba2ebb6fec118bfaa1340466bd47b033e5
fracs31 <pist.fracs95@gmail.com>
2023-02-15 15:16:29 +0100

    ADD array of objects images

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-let images = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"] //array con le immagini del carosello
-let img = document.getElementById("img"); //immagine del carosello
-let btnLeft = document.getElementById("btn-left"); //bottone per andare indietro con le immagini del carosello
-let btnRight = document.getElementById("btn-right"); //bottone per andare avanti con le immagini del carosello
+//Array di immagini
+const images = [
+    {
+        image: "./img/01.webp", //immagine
+        title: "Marvel's Spiderman Miles Morale", //titolo
+        text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.", //testo
+    }, {
+        image: "./img/02.webp", //immagine
+        title: "Ratchet & Clank: Rift Apart", //titolo
+        text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.", //testo
+    }, {
+        image: "./img/03.webp", //immagine
+        title: "Fortnite", //titolo
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.", //testo
+    }, {
+        image: "./img/04.webp", //immagine
+        title: "Stray", //titolo
+        text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city", //testo
+    }, {
+        image: "./img/05.webp", //immagine
+        title: "Marvel's Avengers", //titolo
+        text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.", //testo
+    }
+];
+const imgElement = document.getElementById("img"); //immagine del carosello
+const btnLeft = document.getElementById("btn-left"); //bottone per andare indietro con le immagini del carosello
+const btnRight = document.getElementById("btn-right"); //bottone per andare avanti con le immagini del carosello
 let index = 0; //indice
 
 console.log(images); //stampo l'array con le immagini del carosello
@@ -14,12 +37,12 @@ btnRight.addEventListener("click", function() {
         console.log("Indice prima dell'incremento: " + index); //stampa dell'indice prima dell'incremento
         index++; //incremento l'indice
         console.log("Indice dopo l'incremento: " + index); //stampa dell'indice dopo l'incremento
-        img.src = images[index]; //cambio l'immagine
+        imgElement.src = images[index].image; //cambio l'immagine
     } else if (index == (images.length - 1)) { //altrimenti se l'indice è uguale alla lunghezza dell'array di immagini
         console.log("Indice prima dell'azzeramento: " + index); //stampa dell'indice prima dell'azzeramento
         index = 0; //azzero l'indice
         console.log("Indice dopo l'azzeramento: " + index); //stampa dell'indice dopo dell'azzeramento
-        img.src = images[index]; //cambio l'immagine
+        imgElement.src = images[index].image; //cambio l'immagine
     }
 });
 
@@ -30,11 +53,11 @@ btnLeft.addEventListener("click", function() {
         console.log("Indice prima del decremento: " + index); //stampa dell'indice prima del decremento
         index--; //decremento l'indice
         console.log("Indice dopo il decremento: " + index); //stampa dell'indice dopo il decremento
-        img.src = images[index]; //cambio l'immagine
+        imgElement.src = images[index].image; //cambio l'immagine
     } else if (index == 0) { //altrimenti se l'indice è uguale a 0
         console.log("Indice prima di andare nell'ultima posizione: " + index); //stampa dell'indice prima di andare nell'ultima posizione dell'array di immagini
         index = images.length - 1; //assegno all'indice il valore della lunghezza dell'array di immagini meno 1
         console.log("Indice dopo essere andato nell'ultima posizione: " + index); //stampa dell'indice dopo essere andato nell'ultima posizione dell'array di immagini
-        img.src = images[index]; //cambio l'immagine
+        imgElement.src = images[index].image; //cambio l'immagine
     }
 });
